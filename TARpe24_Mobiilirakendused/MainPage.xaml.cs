acd08f53609b13bad5a593439c64e920a05d2ceb
Opilane TTHK <Saimon.siipan@gmail.com>
2026-02-05 08:41:12 +0200

    Update MainPage.xaml.cs
    tegin esimese Ül ära

--- TARpe24_Mobiilirakendused/MainPage.xaml.cs
+++ TARpe24_Mobiilirakendused/MainPage.xaml.cs
@@ -13,25 +13,45 @@
         {
             count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-            BotImage.Rotation += 20;
-            var rnd = new Random();
-            var rndColor = Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
-            BackgroundColor = rndColor;//
+            // igakord kui nuppu vajutada lisab 0.1 pildi suurusele juurde
+            BotImage.Scale += 0.1;
+            // igakord kui nuppu vajutada võtab nuppu nähtavust 0,1 võrra vähemaks
+            CounterBtn.Opacity -= 0.1;
+
             if (count >= 10)
             {
                 BotImage.IsVisible = false; // Peidab pildi
                 CounterLabel.Text = "Pilt kadus ära! Vajuta Reset.";
             }
-            // Kui loendur on 5 või rohkem, muuda nupu värvi
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            BotImage.Rotation += 20;
+
             if (count >= 5)
             {
                 CounterBtn.BackgroundColor = Colors.Red;
                 CounterBtn.TextColor = Colors.White;
             }
+            else
+            {
+                var rnd = new Random();
+                var rndColor = Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                CounterBtn.BackgroundColor = rndColor;
+            }
+
+            // if operaator mis kontrollib kas count on paarisarv
+            if (count % 2 == 0)
+            {
+                //lisab nurga raadiusele 5 juurde
+                CounterBtn.CornerRadius += 5;
+                //lisab nupu sisse suurust
+                CounterBtn.Padding += 5;
+            }
+
 
 
             SemanticScreenReader.Announce(CounterBtn.Text);
@@ -39,15 +59,14 @@
 
         private void ResetBtn_Clicked(object sender, EventArgs e)
         {
+
             count = 0;
-            CounterBtn.Text = "alustame uuesti!";
-            BotImage.Rotation = 0;//lähtestab pildi pöörde
-            BotImage.IsVisible = true; // Toob pildi tagasi
+            CounterBtn.Text = "Click me";
+            BotImage.IsVisible = true;
+            BotImage.Rotation = 0;
+            BotImage.Scale = 1;
+            CounterBtn.Opacity = 1;
 
-            // Liigutame pildi paremasse serva
-            BotImage.HorizontalOptions = LayoutOptions.End;
-
-            // VÕI teeme loogika: kui on vasakul, liiguta paremale, ja vastupidi
             if (BotImage.HorizontalOptions == LayoutOptions.Start)
             {
                 BotImage.HorizontalOptions = LayoutOptions.End;
